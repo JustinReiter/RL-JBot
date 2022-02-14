6eee60904b20805736b6ea418422f4fc207a7359
Justin Reiter <JDReiter@uwaterloo.ca>
2022-02-13 23:55:00 -0500

    Build training and model logic

--- src/nn_model.py
+++ src/nn_model.py
@@ -1,4 +1,6 @@
 import numpy as np
+import os
+from datetime import datetime
 from tensorflow import keras
 
 class NN_Model:
@@ -6,19 +8,28 @@ class NN_Model:
     def __init__(self):
         self.model = keras.Sequential(
             [
-                keras.layers.Dense(64, input_dim=38, activation='relu'),
-                keras.layers.Dense(56, activation='relu'),
-                keras.layers.Dense(48, activation='relu'),
-                keras.layers.Dense(32, activation='relu'),
+                keras.layers.Dense(128, input_dim=67, activation='relu'),
+                keras.layers.Dense(128, activation='relu'),
+                keras.layers.Dense(128, activation='relu'),
+                keras.layers.Dense(64, activation='relu'),
+                keras.layers.Dense(64, activation='relu'),
                 keras.layers.Dense(8, activation='sigmoid')
             ]
         )
         
         self.model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
 
-    def predict(self, input: np.darray):
-        pass
+    def predict(self, input: np.ndarray):
+        return self.model.predict(input)
 
-    # Similar to self.predict() with adjustment to weights & bias
-    def train(self, input: np.darray):
-        pass
+    # Trains the model using supervised learning
+    def train(self, inputs: np.ndarray, outputs: np.ndarray):
+        self.model.fit(inputs, outputs, epochs=100, batch_size=32)
+    
+    def save_model(self, name=datetime.now().strftime('%Y-%m-%d_%H-%M')):
+        full_path = '{}/../saved_models/{}'.format(os.path.dirname(os.path.realpath(__file__)), name)
+        self.model.save(full_path)
+
+    def load_model(self, name):
+        full_path = '{}/../saved_models/{}'.format(os.path.dirname(os.path.realpath(__file__)), name)
+        self.model = keras.models.load_model(full_path)
--- src/util/load_csvs.py
+++ src/util/load_csvs.py
@@ -1,17 +1,34 @@
-import csv, os
+import os
+import pandas as pd
 
+# loads a single CSV into a list
 def load_file(file_name):
-    with open(file_name, newline='') as fs:
-        return list(csv.reader(fs))
+    return pd.read_csv(file_name).values.tolist()
 
+# loads all CSVs into a single list
 def load_all_files(file_names):
     data = []
     for file_name in file_names:
         data.extend(load_file(file_name))
+    
+    return data
 
+# returns a tuple of input/output file paths sorted in same order
 def get_log_file_names():
+
     files = []
-    for dirpath, _, fileNames in os.walk(os.path.dirname(os.path.realpath(__file__)) + "/../../training/game_data"):
+    for dirpath, _, fileNames in os.walk(os.path.dirname(os.path.realpath(__file__)) + "/../../training/game_data/"):
         for file in fileNames:
             files.append(os.path.abspath(os.path.join(dirpath, file)))
-    return files
+
+    input_files, output_files = [], []
+    for f in files:
+        if 'input' in f:
+            input_files.append(f)
+        else:
+            output_files.append(f)
+    
+    input_files.sort()
+    output_files.sort()
+
+    return input_files, output_files
